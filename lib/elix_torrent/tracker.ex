2309d4640e7e70b716d86b1f4c24ee0f5c954c47
Zander Mackie <zmackie@gmail.com>
2016-10-01 18:01:51 -0400

    Getting from the tracker

--- lib/elix_torrent/tracker.ex
+++ lib/elix_torrent/tracker.ex
@@ -2,13 +2,33 @@ defmodule ElixTorrent.Tracker do
 
   def start(path) do
     {:ok, metafile} = File.read path
-    {:ok, torrent_dict} = Benlixir.Decoder.decode metafile
-
-    make_request({ :ok, torrent_dict })
+    case make_request({ :ok, metafile }) do
+      {:ok, response} -> response.body
+      other -> other
+    end
   end
 
   defp make_request({:ok, torrent_dict}) do
-    {:ok, info} = Bento.Encoder.encode torrent_dict.info
-    info_hash - :crypto.hash(:sha, info)
+    case Bencode.decode_with_info_hash torrent_dict do
+      {:ok, data, checksum} -> get {data, checksum}
+      {:error, reason} -> IO.puts reason
+    end
+  end
+
+  defp get({data, checksum}) do
+    tracker_url = construct_get({data, checksum})
+    HTTPoison.get tracker_url
+  end
+
+  defp construct_get {data, checksum} do
+    params = query_params({data, checksum})
+    announce_uri = URI.parse( data["announce"])
+    query = URI.encode_query(params)
+
+    %URI{announce_uri | query: query}
+  end
+
+  defp query_params({data, checksum}) do
+    [info_hash: checksum, left: data["info"]["length"], peer_id: "-TZ-0000-00000000000"]
   end
 end
--- mix.exs
+++ mix.exs
@@ -10,23 +10,11 @@ defmodule ElixTorrent.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.9.0"}, {:benlixir, "~> 0.1.7", hex: :bendy}, {:bento, "~> 0.9.2"}]
+    [{:httpoison, "~> 0.9.0"}, {:bencode, "~> 0.3.0"}]
   end
 end
